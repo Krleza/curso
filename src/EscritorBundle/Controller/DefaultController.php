5eb309e85af32721b617278397b3d424b78764e2
Pablo Carrillo <pablo@pablocarrillo.net>
2016-02-14 13:24:35 +0100

    peticion de autentifiacion por hhtp

--- src/EscritorBundle/Controller/DefaultController.php
+++ src/EscritorBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace EscritorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,13 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('EscritorBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/admin")
+     */
+    public function adminAction()
+    {
+        return new Response('<html><body>Pagina con clave</body></html>');
     }
 }
